b1a77e19f9266d54f6ba4c8df9aabc9c2850e42b
Shichong Pengde <shichong.peng@mail.utoronto.ca>
2019-09-17 13:28:15 -0700

    add cicada test script

---
+++ scripts/rrdb_test_cicada_0.sh
@@ -0,0 +1,45 @@
+set -ex
+# models
+MODEL='bicycle_gan'
+RESULTS_DIR='./results/rrdb_test_cicada_0'
+#CHECKPOINTS_DIR='../checkpoints/n02279972_BGAN/n02279972_BGAN_bicycle_gan/'
+#CHECKPOINTS_DIR='./checkpoints/n01531178_BGAN_x8/'
+
+# dataset
+CLASS='Cicada_RRDB_BGAN_x8_5'
+CHECKPOINTS_DIR='./checkpoints'
+NZ=5
+NO_FLIP='--no_flip'
+DIRECTION='AtoB'
+LOAD_SIZE=256
+CROP_SIZE=256
+INPUT_NC=3
+NAME=${CLASS}_${MODEL}
+
+OPT=./train_srim_cicada_x8.json
+G_NET='IMRRDB_net'
+
+# misc
+GPU_ID=1  # gpu id
+NUM_TEST=20 # number of input images duirng test
+NUM_SAMPLES=50 # number of samples per input images
+DISPLAY_ID=0
+
+# command
+python ./test_rrdb.py \
+  --dataroot ./datasets/${CLASS} \
+  --results_dir ${RESULTS_DIR} \
+  --checkpoints_dir ./checkpoints/${CLASS} \
+  --name ${NAME} \
+  --nz ${NZ} \
+  --direction ${DIRECTION} \
+  --load_size ${LOAD_SIZE} \
+  --crop_size ${CROP_SIZE} \
+  --input_nc ${INPUT_NC} \
+  --num_test ${NUM_TEST} \
+  --n_samples ${NUM_SAMPLES} \
+  --no_flip \
+  --gpu_ids ${GPU_ID} \
+  --opt ${OPT} \
+  --netG ${G_NET} \
+  --upsample bilinear
