5015d4eb6a344791c19a814418def10bd7c5bfc8
Shichong Pengde <shichong.peng@mail.utoronto.ca>
2019-09-26 22:22:51 -0700

    adding data creation script

---
+++ util/create_hr_lr_lmdb.py
@@ -0,0 +1,244 @@
+import sys
+import os.path
+import glob
+import pickle
+import lmdb
+import cv2
+import torch
+import math
+import numpy as np
+
+sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
+from utils.progress_bar import ProgressBar
+
+# configurations
+# img_folder = '/home/nio/local/hdd2/data/n01531178/*'  # glob matching pattern
+
+img_folder = '/home/nio/local/ssd1/data/imagenet/raw/n07745940/*'  # glob matching pattern
+lmdb_save_path = '/home/nio/local/ssd1/data/imagenet/n07745940.lmdb'  # must end with .lmdb
+down_lmdb_save_path = '/home/nio/local/ssd1/data/imagenet/n07745940_down_8.lmdb'
+#
+# img_folder = '/home/nio/local/ssd1/data/imagenet/raw/n02256656_test/*'  # glob matching pattern
+# lmdb_save_path = '/home/nio/local/ssd1/data/imagenet/raw/n02256656_test.lmdb'  # must end with .lmdb
+# down_lmdb_save_path = '/home/nio/local/ssd1/data/imagenet/n02256656_test_down_8.lmdb'
+
+# down_lmdb_save_path = '/home/nio/local/hdd2/data/n01531178_down_4.lmdb'
+
+img_list = sorted(glob.glob(img_folder))
+dataset = []
+
+scale = 1. / 8
+# scale = 1. / 4
+down_dataset = []
+
+data_size = 0
+down_data_size = 0
+
+
+def cubic(x):
+    absx = torch.abs(x)
+    absx2 = absx**2
+    absx3 = absx**3
+    return (1.5*absx3 - 2.5*absx2 + 1) * ((absx <= 1).type_as(absx)) + \
+        (-0.5*absx3 + 2.5*absx2 - 4*absx + 2) * (((absx > 1)*(absx <= 2)).type_as(absx))
+
+
+def calculate_weights_indices(in_length, out_length, scale, kernel, kernel_width, antialiasing):
+    if (scale < 1) and (antialiasing):
+        # Use a modified kernel to simultaneously interpolate and antialias- larger kernel width
+        kernel_width = kernel_width / scale
+
+    # Output-space coordinates
+    x = torch.linspace(1, out_length, out_length)
+
+    # Input-space coordinates. Calculate the inverse mapping such that 0.5
+    # in output space maps to 0.5 in input space, and 0.5+scale in output
+    # space maps to 1.5 in input space.
+    u = x / scale + 0.5 * (1 - 1 / scale)
+
+    # What is the left-most pixel that can be involved in the computation?
+    left = torch.floor(u - kernel_width / 2)
+
+    # What is the maximum number of pixels that can be involved in the
+    # computation?  Note: it's OK to use an extra pixel here; if the
+    # corresponding weights are all zero, it will be eliminated at the end
+    # of this function.
+    P = math.ceil(kernel_width) + 2
+
+    # The indices of the input pixels involved in computing the k-th output
+    # pixel are in row k of the indices matrix.
+    indices = left.view(out_length, 1).expand(out_length, P) + torch.linspace(0, P - 1, P).view(
+        1, P).expand(out_length, P)
+
+    # The weights used to compute the k-th output pixel are in row k of the
+    # weights matrix.
+    distance_to_center = u.view(out_length, 1).expand(out_length, P) - indices
+    # apply cubic kernel
+    if (scale < 1) and (antialiasing):
+        weights = scale * cubic(distance_to_center * scale)
+    else:
+        weights = cubic(distance_to_center)
+    # Normalize the weights matrix so that each row sums to 1.
+    weights_sum = torch.sum(weights, 1).view(out_length, 1)
+    weights = weights / weights_sum.expand(out_length, P)
+
+    # If a column in weights is all zero, get rid of it. only consider the first and last column.
+    weights_zero_tmp = torch.sum((weights == 0), 0)
+    if not math.isclose(weights_zero_tmp[0], 0, rel_tol=1e-6):
+        indices = indices.narrow(1, 1, P - 2)
+        weights = weights.narrow(1, 1, P - 2)
+    if not math.isclose(weights_zero_tmp[-1], 0, rel_tol=1e-6):
+        indices = indices.narrow(1, 0, P - 2)
+        weights = weights.narrow(1, 0, P - 2)
+    weights = weights.contiguous()
+    indices = indices.contiguous()
+    sym_len_s = -indices.min() + 1
+    sym_len_e = indices.max() - in_length
+    indices = indices + sym_len_s - 1
+    return weights, indices, int(sym_len_s), int(sym_len_e)
+
+
+def imresize_np(img, scale, antialiasing=True):
+    # Now the scale should be the same for H and W
+    # input: img: Numpy, HWC BGR [0,1]
+    # output: HWC BGR [0,1] w/o round
+    img = torch.from_numpy(img)
+
+    in_H, in_W, in_C = img.size()
+    out_C, out_H, out_W = in_C, math.ceil(in_H * scale), math.ceil(in_W * scale)
+    kernel_width = 4
+    kernel = 'cubic'
+
+    # Return the desired dimension order for performing the resize.  The
+    # strategy is to perform the resize first along the dimension with the
+    # smallest scale factor.
+    # Now we do not support this.
+
+    # get weights and indices
+    weights_H, indices_H, sym_len_Hs, sym_len_He = calculate_weights_indices(
+        in_H, out_H, scale, kernel, kernel_width, antialiasing)
+    weights_W, indices_W, sym_len_Ws, sym_len_We = calculate_weights_indices(
+        in_W, out_W, scale, kernel, kernel_width, antialiasing)
+    # process H dimension
+    # symmetric copying
+    img_aug = torch.FloatTensor(in_H + sym_len_Hs + sym_len_He, in_W, in_C)
+    img_aug.narrow(0, sym_len_Hs, in_H).copy_(img)
+
+    sym_patch = img[:sym_len_Hs, :, :]
+    inv_idx = torch.arange(sym_patch.size(0) - 1, -1, -1).long()
+    sym_patch_inv = sym_patch.index_select(0, inv_idx)
+    img_aug.narrow(0, 0, sym_len_Hs).copy_(sym_patch_inv)
+
+    sym_patch = img[-sym_len_He:, :, :]
+    inv_idx = torch.arange(sym_patch.size(0) - 1, -1, -1).long()
+    sym_patch_inv = sym_patch.index_select(0, inv_idx)
+    img_aug.narrow(0, sym_len_Hs + in_H, sym_len_He).copy_(sym_patch_inv)
+
+    out_1 = torch.FloatTensor(out_H, in_W, in_C)
+    kernel_width = weights_H.size(1)
+    for i in range(out_H):
+        idx = int(indices_H[i][0])
+        out_1[i, :, 0] = img_aug[idx:idx + kernel_width, :, 0].transpose(0, 1).mv(weights_H[i])
+        out_1[i, :, 1] = img_aug[idx:idx + kernel_width, :, 1].transpose(0, 1).mv(weights_H[i])
+        out_1[i, :, 2] = img_aug[idx:idx + kernel_width, :, 2].transpose(0, 1).mv(weights_H[i])
+
+    # process W dimension
+    # symmetric copying
+    out_1_aug = torch.FloatTensor(out_H, in_W + sym_len_Ws + sym_len_We, in_C)
+    out_1_aug.narrow(1, sym_len_Ws, in_W).copy_(out_1)
+
+    sym_patch = out_1[:, :sym_len_Ws, :]
+    inv_idx = torch.arange(sym_patch.size(1) - 1, -1, -1).long()
+    sym_patch_inv = sym_patch.index_select(1, inv_idx)
+    out_1_aug.narrow(1, 0, sym_len_Ws).copy_(sym_patch_inv)
+
+    sym_patch = out_1[:, -sym_len_We:, :]
+    inv_idx = torch.arange(sym_patch.size(1) - 1, -1, -1).long()
+    sym_patch_inv = sym_patch.index_select(1, inv_idx)
+    out_1_aug.narrow(1, sym_len_Ws + in_W, sym_len_We).copy_(sym_patch_inv)
+
+    out_2 = torch.FloatTensor(out_H, out_W, in_C)
+    kernel_width = weights_W.size(1)
+    for i in range(out_W):
+        idx = int(indices_W[i][0])
+        out_2[:, i, 0] = out_1_aug[:, idx:idx + kernel_width, 0].mv(weights_W[i])
+        out_2[:, i, 1] = out_1_aug[:, idx:idx + kernel_width, 1].mv(weights_W[i])
+        out_2[:, i, 2] = out_1_aug[:, idx:idx + kernel_width, 2].mv(weights_W[i])
+
+    return out_2.numpy()
+
+
+print('Read images...')
+pbar = ProgressBar(len(img_list))
+for i, v in enumerate(img_list):
+    pbar.update('Read {}'.format(v))
+    img = cv2.imread(v, cv2.IMREAD_UNCHANGED)
+    if len(img.shape) < 3:
+        img = cv2.cvtColor(img, cv2.COLOR_GRAY2BGR)
+    dataset.append(img)
+    down_img = imresize_np(img, scale, True)
+    down_dataset.append(down_img)
+    # print(img.shape, down_img.shape)
+    data_size += img.nbytes
+    down_data_size += down_img.nbytes
+env = lmdb.open(lmdb_save_path, map_size=data_size * 10)
+down_env = lmdb.open(down_lmdb_save_path, map_size=down_data_size * 10)
+print('Finish reading {} images.\nWrite lmdb...'.format(len(img_list)))
+
+pbar = ProgressBar(len(img_list))
+with env.begin(write=True) as txn:  # txn is a Transaction object
+    for i, v in enumerate(img_list):
+        pbar.update('Write {}'.format(v))
+        base_name = os.path.splitext(os.path.basename(v))[0]
+        key = base_name.encode('ascii')
+        data = dataset[i]
+        if dataset[i].ndim == 2:
+            H, W = dataset[i].shape
+            C = 1
+        else:
+            H, W, C = dataset[i].shape
+        meta_key = (base_name + '.meta').encode('ascii')
+        meta = '{:d}, {:d}, {:d}'.format(H, W, C)
+        # The encode is only essential in Python 3
+        txn.put(key, data)
+        txn.put(meta_key, meta.encode('ascii'))
+print('Finish writing lmdb.')
+
+# create keys cache
+keys_cache_file = os.path.join(lmdb_save_path, '_keys_cache.p')
+env = lmdb.open(lmdb_save_path, readonly=True, lock=False, readahead=False, meminit=False)
+with env.begin(write=False) as txn:
+    print('Create lmdb keys cache: {}'.format(keys_cache_file))
+    keys = [key.decode('ascii') for key, _ in txn.cursor()]
+    pickle.dump(keys, open(keys_cache_file, "wb"))
+print('Finish creating lmdb keys cache.')
+
+
+pbar = ProgressBar(len(img_list))
+with down_env.begin(write=True) as txn:  # txn is a Transaction object
+    for i, v in enumerate(img_list):
+        pbar.update('Write {}'.format(v))
+        base_name = os.path.splitext(os.path.basename(v))[0]
+        key = base_name.encode('ascii')
+        data = down_dataset[i]
+        if down_dataset[i].ndim == 2:
+            H, W = down_dataset[i].shape
+            C = 1
+        else:
+            H, W, C = down_dataset[i].shape
+        meta_key = (base_name + '.meta').encode('ascii')
+        meta = '{:d}, {:d}, {:d}'.format(H, W, C)
+        # The encode is only essential in Python 3
+        txn.put(key, data.astype(np.uint8))
+        txn.put(meta_key, meta.encode('ascii'))
+    print(data.shape)
+print('Finish writing down_lmdb.')
+
+# create keys cache
+keys_cache_file = os.path.join(down_lmdb_save_path, '_keys_cache.p')
+down_env = lmdb.open(down_lmdb_save_path, readonly=True, lock=False, readahead=False, meminit=False)
+with down_env.begin(write=False) as txn:
+    print('Create lmdb keys cache: {}'.format(keys_cache_file))
+    keys = [key.decode('ascii') for key, _ in txn.cursor()]
+    pickle.dump(keys, open(keys_cache_file, "wb"))
+print('Finish creating down_lmdb keys cache.')
